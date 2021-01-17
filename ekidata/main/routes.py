e88e40f365d6af53611144371c985384e295086f
Gary Sentosa <gary.sentosa@gmail.com>
2021-01-17 06:47:39 +0000

    Let static index.html served by other web server

--- ekidata/main/routes.py
+++
@@ -1,8 +0,0 @@
-from ekidata.main import bp
-
-
-@bp.route('/', defaults={'path': ''})
-@bp.route('/<string:path>')
-@bp.route('/<path:path>')
-def catch_all(path):
-    return bp.send_static_file('index.html')
